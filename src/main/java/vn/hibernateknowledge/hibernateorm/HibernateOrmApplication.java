d04ebde69e7bbafe5254bc75fe3a2575c43e05d8
Tran Quang Minh <minh.tran.1@swisspost.com>
2022-03-24 23:55:55 +0700

    init hibernate source code

---
+++ src/main/java/vn/hibernateknowledge/hibernateorm/HibernateOrmApplication.java
@@ -0,0 +1,13 @@
+package vn.hibernateknowledge.hibernateorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateOrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/hibernateknowledge/hibernateorm/HibernateOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.hibernateknowledge.hibernateorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
